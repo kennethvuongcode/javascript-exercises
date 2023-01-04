a08ae43e63fce6940301a3cb0692d10cbd00545a
Kenneth Vuong <kennethvuongcode@gmail.com>
2023-01-04 00:14:47 -0800

    finished exercises 8 and 9!

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(x,y) {
+  return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x,y) {
+  return x - y;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((total, currentValue) => total + currentValue, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+	return array.reduce((total, currentValue) => total * currentValue, 1);
 };
 
-const power = function() {
-	
+const power = function(x,y) {
+	return x ** y;
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+	if (x == 0) return 1;
+  let counter = x;
+
+  while (x > 1){
+    x--;
+    counter *= x;
+  }
+
+  return counter;
 };
 
 // Do not edit below this line
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,35 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    str = str.replace(/\s+/g, '');
+    let palCheck = filterNormalChar(str.split(''));
 
+    for (let i = 0; i < (palCheck.length/2) + 1; i++) {
+        if (palCheck[i] != palCheck[(palCheck.length-i)-1])
+            return false;
+    }
+    return true;
 };
+
+function filterNormalChar(array) {
+    for (let i = 0; i < array.length; i++) {
+        if (isLetter(array[i]) == false)
+            array.splice(i,1);
+    }
+
+    array = array.join('');
+    array = array.toLowerCase();
+
+    return array;
+} 
+
+
+
+function isLetter(str) {
+    if (str.length === 1 && str.match(/[a-z]/i)) {
+        return true;
+    }     
+    
+    return false;  
+}
 
 // Do not edit below this line
 module.exports = palindromes;
